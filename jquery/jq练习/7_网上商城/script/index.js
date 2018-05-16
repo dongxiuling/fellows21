05f8ac81d679ce9398da39632a87ebad229d54c3
dongxiuling <dxl_web@163.com>
2018-05-16 15:10:56 +0800

    jq

--- jquery/jq练习/7_网上商城/script/index.js
+++ jquery/jq练习/7_网上商城/script/index.js
@@ -1,3 +1,37 @@
 /**
  * Created by apple on 18/5/13.
  */
+var count = 0;
+var timer ;
+$('.btn-box a').on('mouseover',function(){
+    change($(this).index());
+    count = $(this).index();
+});
+run();
+
+$('#jnImageroll').on('mouseover',function(){
+    clearInterval(timer);
+}).on('mouseout',function(){
+   run();
+});
+function run(){
+    timer = setInterval(function(){
+        count++;
+        if(count == $('#JS_imgWrap img').length){
+            count = 0;
+        }
+        change(count);
+
+    },2000);
+}
+function change(idx){
+    $('.btn-box a').eq(idx).addClass('chos').siblings().removeClass('chos');
+    $('#JS_imgWrap img').eq(idx).stop().fadeIn(600).siblings().stop().fadeOut(600);
+}
+
+
+
+
+
+
+
